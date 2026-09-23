c6c0aaf25404c6a8ad5bd8aca562628d2233c13c
Theo1 <theo1laris@icloud.com>
2026-09-23 13:45:16 +0200

    Implémentation de la homepage

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+require_once 'shared/OpenHtml.php';
+?>
+
+<main>
+    <p>Découvrez votre quotient intellectuel grâce à notre test.</p>
+
+    <a href="question1.php">Commencer le test</a>
+</main>
+
+<?php
+require_once 'shared/CloseHtml.php';
+?>
---
+++ shared/OpenHtml.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TestQI</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+
+<header>
+    <img src="images/iq.jpeg" alt="Logo TestQI">
+    <h1>TestQI</h1>
+</header>
